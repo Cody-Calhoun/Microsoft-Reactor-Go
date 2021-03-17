dc91dc49333d059e61e7de5db7b51d092cf034ff
Will <w.calhoun@outlook.com>
2021-03-16 22:34:36 -0700

    Microsoft Lecture Part 1

---
+++ src/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("Second Hello World!")
+	red()
+
+}
+func red() {
+	fmt.Println("My favorite Color is red!")
+}
